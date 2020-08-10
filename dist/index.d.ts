947012abe2329dc21b2bcd8e0cf40dd9d5bbaaff
Geoff Lawson <jeffwithg@gmail.com>
2020-08-10 13:48:22 -0700

    removes types from js

---
+++ dist/index.d.ts
@@ -0,0 +1,51 @@
+interface EventHandler {
+    (event: Event, args: string): void;
+}
+interface FuncBool {
+    (param?: any): boolean;
+}
+interface FuncVoid {
+    (param?: any): void;
+}
+interface FuncString {
+    (param?: any): string;
+}
+interface On {
+    (event: string, callback: EventHandler): void;
+}
+interface App {
+    requestSingleInstanceLock: FuncBool;
+    isDefaultProtocolClient: FuncBool;
+    setAsDefaultProtocolClient: FuncVoid;
+    quit: FuncVoid;
+    on: On;
+    getAppPath: FuncString;
+}
+interface MainWindow {
+    isMinimized: FuncBool;
+    restore: FuncVoid;
+    focus: FuncVoid;
+}
+interface DeeplinkConfig {
+    protocol: string;
+    app: App;
+    mainWindow: MainWindow;
+    isDev?: boolean;
+    isYarn?: boolean;
+    debugLogging?: boolean;
+}
+declare class Deeplink {
+    events?: any;
+    private appPath?;
+    private electronPath?;
+    private infoPlistFile?;
+    private infoPlistFileBak?;
+    private config;
+    constructor(config: DeeplinkConfig);
+    private checkConfig;
+    private createHandlerApp;
+    private emitter;
+    restoreInfoPlist: () => void;
+    getProtocol: () => string;
+}
+export { Deeplink };
